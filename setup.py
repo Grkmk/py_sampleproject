61d05f3839c265959e96189379c9df073c575cff
gorkem <gorkem.kockaya@gmail.com>
2020-04-27 20:40:48 +0300

    setup.py updated

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 import setuptools
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setuptools.setup(
     name="django-polls-GRKMK",
     version="0.0.1",
     author="Gorkem Kockaya",
     author_email="gorkem@example.com",
     description="A Django app to conduct Web-based polls.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
     url="https://github.com/grkmk/py_sampleproject",
     packages=setuptools.find_packages(),
     classifiers=[
